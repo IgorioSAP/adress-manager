c6536ebfde6c24c9e7ecbc1aa6621342d7156b6f
Igor Rivchin <igor.rivchin@gmail.com>
2018-11-27 11:58:23 +0100

    Multitenancy

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/CreateAddressCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/CreateAddressCommand.java
@@ -1,10 +1,10 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
-import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
-import com.sap.cloud.sdk.s4hana.connectivity.ErpCommand;
 import org.slf4j.Logger;
 
 import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
+import com.sap.cloud.sdk.s4hana.connectivity.ErpCommand;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 
@@ -17,17 +17,18 @@ public class CreateAddressCommand extends ErpCommand<BusinessPartnerAddress> {
     public CreateAddressCommand(final BusinessPartnerService service, final BusinessPartnerAddress addressToCreate) {
         super(HystrixUtil.getDefaultErpCommandSetter(
                 CreateAddressCommand.class,
-                HystrixUtil.getDefaultErpCommandProperties()
-                .withExecutionTimeoutInMilliseconds(10000)  //10 Seconds für den eigenen Thread, Falls kein Fallback, dann gibt es keine weitere Option, falls es schief läuft
-        ));
+                HystrixUtil.getDefaultErpCommandProperties().withExecutionTimeoutInMilliseconds(10000)));
+
         this.service = service;
         this.addressToCreate = addressToCreate;
     }
 
     @Override
     protected BusinessPartnerAddress run() throws Exception {
-        // TODO: Replace with Virtual Data Model query
-        return service.createBusinessPartnerAddress(addressToCreate).execute();
-       // return null;
+        final BusinessPartnerAddress addressCreated = service
+                .createBusinessPartnerAddress(addressToCreate)
+                .execute();
+
+        return addressCreated;
     }
 }
